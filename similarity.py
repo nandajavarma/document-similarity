be4d82d10bb3e2cafd6d6e74915ddd101c84d2ab
Nandaja Varma <nandaja.varma@gmail.com>
2017-03-21 14:46:00 -0400

    Quora kaggle solution stub

--- similarity.py
+++ similarity.py
@@ -2,16 +2,21 @@ from create_vector import vectorize
 from lsh import LSH
 from parse_document import extract_from_pdf
 import sys
+import pandas as pd
 
 if __name__ == "__main__":
-    pdfs = sys.argv[1:]
-    if pdfs:
-        vector_list = [vectorize(extract_from_pdf(pdf)) for pdf in pdfs]
+    csv = sys.argv[1:]
+    if csv:
+        questions = pd.read_csv(csv)
+        question1 = questions(2)
+        question2 = questions(3)
+        vector_list = [vectorize(question1), vectorize(question2)]
     else:
         print "Usage: python create_vector.py pdf1 [pdf2] [pdf3] .."
         sys.exit()
-    lsh = LSH(300)
-    [lsh.insert_document(title, vector) for (title, vector) in vector_list]
-    # print lsh.get_similarities()
-    print lsh.closest_match(vector_list[0][0])
+    print vector_list
+    # lsh = LSH(300)
+    # [lsh.insert_document(title, vector) for (title, vector) in vector_list]
+    # # print lsh.get_similarities()
+    # print lsh.closest_match(vector_list[0][0])
     # return vector_list
